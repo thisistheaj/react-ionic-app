eb6ea0940d716cda8a70680bc25f9753ea01965c
thisistheaj <ajbeckner@gmail.com>
2019-04-20 04:34:58 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+
+import {
+    IonApp,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonButton,
+} from '@ionic/react';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <IonApp>
+                <IonContent padding>
+                    <IonCard>
+                        <IonCardHeader>
+                            <IonCardSubtitle>Welcome to Ionic</IonCardSubtitle>
+                            <IonCardTitle>Running on React</IonCardTitle>
+                        </IonCardHeader>
+                    </IonCard>
+                    <IonButton onClick={() => {alert('foo')}}>
+                        Click
+                    </IonButton>
+                </IonContent>
+            </IonApp>
+        );
+    }
 }
 
 export default App;
